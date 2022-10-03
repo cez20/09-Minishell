4caeed66373b3a8c70ed14e0070fecd8439776cc
Cesar Menjivar <cemenjiv@student.42quebec.com>
2022-10-03 14:46:35 -0400

    Break down de var_expansion en plusieurs petites fonctions

--- var_expansion.c
+++ var_expansion.c
@@ -6,15 +6,12 @@
 /*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 09:58:57 by cemenjiv          #+#    #+#             */
-/*   Updated: 2022/09/30 15:20:50 by cemenjiv         ###   ########.fr       */
+/*   Updated: 2022/10/03 14:45:10 by cemenjiv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "Libft/libft.h"
-
+#include <string.h>
 
 /* Gerer les cas d'erreurs:
 - (FAIT) Si le premier caractere du token n'est pas '$', ne rien faire. Le token, reste tel quel.
@@ -25,9 +22,11 @@
 - Si le contenu est $? = retourner l'exit number. Dans tous les autres cas, ne rien faire.  
 */
 
+/* Fonction qui circule dans ENV, et retourn le contenu qui se trouve
+apres le signe (=);*/
 char	*new_expanded_variable(int i, char *str, char **env)
 {
-	int j;
+	int	j;
 
 	j = 0;
 	while (env[i][j] == str[j] && str[j])
@@ -36,85 +35,58 @@ char	*new_expanded_variable(int i, char *str, char **env)
 	return (str);
 }
 
-// char	*var_expansion(char **str, char **env)
-// {
-// 	char *str1; // string qui contient contenu avant le dollar sign 
-// 	char *str2; // string qui contient contenu apres le dollar sign 
-// 	char *str3; // String qui contient contient contenu apres dollar sign + "="
-// 	int i;
-
-// 	i = 0;
-// 	while ((*str)[i] != '$')
-// 		i++;
-// 	if (i > 0)
-// 		str3 = ft_substr(*str, 0, i);
-// 	if ((*str)[i] == '$')	
-// 	{
-// 		i++;
-// 		str1 = ft_strdup((*str + i));
-// 		str2 = ft_strjoin(str1, "=");
-// 		i = 0;
-// 		while (env[i] != NULL && ft_strnstr(env[i], str2, ft_strlen(str2)) == 0)
-// 		  	i++;
-// 		printf("La variable i est d'une valeur de : %d\n", i);
-// 		if (env[i] != NULL)
-// 		{
-// 			if (str3)
-// 			{
-// 				free(*str);	
-// 				*str = ft_strjoin(str3, new_expanded_variable(i, str2, env));
-// 			}
-// 			else
-// 				*str = new_expanded_variable(i, str2, env);
-// 		}
-// 		free(str1);
-// 		free(str2);
-// 		free(str3);
-// 		printf("Le token final une fois l'expansion fait est: %s\n", *str);
-// 	}
-// 	return (*str);
-// }
-
-void	remplace_string(char *str, char *str1, char *str2, char **env)
+/* Fonction qui trouve l'expansion dans ENV et appelle une autre fonction
+pour changer l'expansion par son contenu
+1- Je circule dans env, jusqu'a trouver la bonne ligne correspondant a expansion
+2- Une fois, dans la bonne ligne, je rentre dans une condition:
+	a) S'il y un str1(contenu avec) expansion, str va pointer un string qui joint
+		str1 et le contenu de la variable d'expansion
+	b) Sinon, str va pointer vers le contenu de la variable d'expansion */
+void	find_expansion(char **str, char *str1, char *str2, char **env)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while (env[i] != NULL && ft_strnstr(env[i], str2, ft_strlen(str2)) == 0)
+	while (env[i] && ft_strnstr(env[i], str2, ft_strlen(str2)) == 0)
 		i++;
-	if (env[i] != NULL)
+	if (env[i])
 	{
+		free(*str);
 		if (str1)
-		{
-			free(str);	
-			str = ft_strjoin(str1, new_expanded_variable(i, str2, env));
-		}
+			*str = ft_strjoin(str1, new_expanded_variable(i, str2, env));
 		else
-			str = new_expanded_variable(i, str2, env);
+			*str = ft_strdup(new_expanded_variable(i, str2, env));
 	}
-	printf("Le token final une fois l'expansion fait est: %s\n", str);
 }
 
-
+// Fonction qui traire l'expansion avec dollar sign($)
+// 1- On circule dans la string jusqu'a renconter un $. 
+// 2- Si applicable, on copie le contenu de la string anterieur 
+// au dollar sign (ex: echo$ARGS copiera "echo") dans str1
+// 3- On copie le contenu se trouvant apres le dollar sign dans un nouvelle
+// 	string (str2) qui est malloc avec strdup
+// 4- On creer une nouvelle string en fusionant str2(ARGS) et "=" 
+//    pour donner "ARGS="
+// 5- J'envoie le tout a la fonction find_expansion qui fera la substition 
 char	*var_expansion(char **str, char **env)
 {
-	char *str1; // string qui contient contenu avant le dollar sign 
-	char *str2; // string qui contient contenu apres le dollar sign 
-	char *str3; // String qui contient contient contenu apres dollar sign + "="
-	int i;
+	char	*str1;
+	char	*str2;
+	char	*str3;
+	int		i;
 
 	i = 0;
 	str1 = NULL;
 	while ((*str)[i] != '$' && (*str)[i])
 		i++;
-	if ((*str)[i] == '$')	
+	if ((*str)[i] == '$')
 	{
 		if (i > 0)
 			str1 = ft_substr(*str, 0, i);
 		i++;
 		str2 = ft_strdup((*str + i));
 		str3 = ft_strjoin(str2, "=");
-		remplace_string(*str, str1, str3, env);
+		find_expansion(str, str1, str3, env);
 		free(str1);
 		free(str2);
 		free(str3);
@@ -122,16 +94,22 @@ char	*var_expansion(char **str, char **env)
 	return (*str);
 }
 
-
-int main(int argc, char **argv, char **env)
+/* Petite fonction main temporaire, d'ici le moment ou 
+on trouve ou l'inserer dans notre programme
+Je cree un *str qui represente l'equivalent d'un token
+qui contient un variable d'environnement ($). Le "free"
+final sera "normalement" fait lorsqu'on va free tous les 
+tokens. */
+int	main(int argc, char **argv, char **env)
 {
+	char	*str;
+
 	(void)argc;
 	(void)argv;
-	char *str;
-	
 	str = malloc(10 * sizeof(char));
-	strcpy(str, "echoARGS");
+	strcpy(str, "echo$ARGS");
 	var_expansion(&str, env);
 	printf("Le token final est: %s\n", str);
-	free(str); // Seulement si la premiere lettre du string n'est pas $
+	free(str);
+	return (0);
 }
