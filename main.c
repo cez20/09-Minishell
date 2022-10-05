e2064d95b3317131b855e913524d6ae85ead2c53
Steven-emmanuel Lavoie <slavoie@c3r6p4.42quebec.com>
2022-10-05 15:50:55 -0400

    com

--- main.c
+++ main.c
@@ -6,20 +6,11 @@
 /*   By: slavoie <slavoie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 14:50:27 by slavoie           #+#    #+#             */
-/*   Updated: 2022/10/05 15:32:24 by slavoie          ###   ########.fr       */
+/*   Updated: 2022/10/05 15:48:39 by slavoie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// void	print_tab(char **tab)
-// {
-// 	while (*tab)
-// 	{
-// 		printf("str = %s\n", *tab);
-// 		tab++;
-// 	}
-// }
 
 char	*get_path(char *command, char **envp)
 {
@@ -76,126 +67,58 @@ void	command_exeggutor(char *argv, char **envp)
 		yo_its_wrong("echec exec\n");
 }
 
-
-// void	echo(char **info->token)
-// {
-// 	int i;
-
-// 	if (!ft_strncmp(info->token[1], "-n", 2))
-// 		i = 2;
-// 	else
-// 		i = 1;
-
-// 	while(info->token[i])
-// 	{
-// 		printf("%s ", info->token[i++]);
-// 	}
-// 	if (ft_strncmp(info->token[1], "-n", 2))
-// 		printf("\n");
-	
-// }
-
-char	*search_line(char **tab, char *search)
+/*
+	cherche la ligne (char *line) dans le tableau (char **tab) et la renvoie
+	si rien n'est trouvé, renvoie NULL 
+*/
+char	*search_line(char **tab, char *line)
 {
 	while(*tab)
 	{
-		if (ft_strnstr(*tab, search, ft_strlen(search)))
+		if (ft_strnstr(*tab, line, ft_strlen(line)))
 			return(*tab);
 		tab++;
 	}
 	return (NULL);
 }
 
-// void	pwd(char **envp)
-// {
-// 	char *pwd;
-
-// 	pwd = search_line(envp, "PWD=");
-// 	pwd = ft_strchr(pwd, '=');
-// 	printf("%s\n", ++pwd);
-// }
-
+/*
+	exécute le builtin associer à la première commande
+*/
 void token_manager(t_info *info)
 {
-	// printf("info->token[0] = %s\n", info->token[0]);
-	// printf("info->token[1] = %s\n", info->token[1]);
-	// printf("len = %zu\n", ft_strlen(info->token[0]));
 	if (!ft_strncmp(info->token[0], "echo", 4))
 		echo(info);
 	if (!ft_strncmp(info->token[0], "pwd", 3))
 		pwd(info);
 	if (!ft_strncmp(info->token[0], "env", 3))
 		print_tab(info->envp);
-
 }
 
+/*
+	Vérifier la valeur du char à l'adresse du char pointeur "token"
+	renvoie la valeur du char si c'est '' ou ""
+	sinon renvoie la valeur de l'espace soit '32' 
+*/
 char simple_or_double(char *token)
 {
 	if (*token == 34 || *token == 39)
 		return (*token);
 	return (32);
-
-
-
-
-	// while(*token)
-	// {
-	// 	// printf("char = %d\n", *token);
-	// 	if (*token == 34 || *token == 39)
-	// 		return (*token);
-	// 	token++;
-	// }
-	// return (0);
-}
-// int	tab_length(char **tab)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (tab[i])
-// 		i++;
-// 	return (i);
-// }
-
-// char	**tab_join(char **tab, char *line)
-// {
-// 	int		len;
-// 	char	**new_tab;
-// 	int		i;
-
-// 	i = 0;
-// 	if (!tab)
-// 	{
-// 		new_tab = ft_calloc(2, sizeof (char *));
-// 		new_tab[i++] = ft_strdup(line);
-// 		new_tab[i] = NULL;
-// 		return (new_tab);
-// 	}
-// 	len = tab_length(tab);
-// 	new_tab = ft_calloc ((len + 2), sizeof(char *));
-// 	while (i < len)
-// 	{
-// 		new_tab[i] = ft_strdup(tab[i]);
-// 		i++;
-// 	}
-// 	new_tab[i++] = ft_strdup(line);
-// 	new_tab[i] = 0;
-// 	table_flip(tab);
-// 	return (new_tab);
-// }
-
-void	add_token(t_info *info, char *token)
-{
-	tab_join(info->token, token);
 }
 
+/*
+	Avance le pointeur jusqu'à la prchaine chose à interpréter
+*/
 void skip_space(t_info *info)
 {
 	while(is_white_space(*info->last_position))
 		info->last_position++;
-	// printf("position = %c\n", *info->last_position);
 }
 
+/*
+	Cherche la prochaine occurence de "c" et renvoie le token
+*/
 char	*search_another_one(char *str, char c, t_info *info)
 {
 	char	*token;
@@ -217,47 +140,11 @@ char	*search_another_one(char *str, char c, t_info *info)
 			token = ft_substr(start, 0, len);
 			return (token);
 		}
-		// printf("char = %c\n", *str);
 	}
 	len++;
 	info->last_position = ++str;
 	token = ft_substr(start, 0, len);
-	// printf("token = %s\n", token);
 	return (token);
-
-	// char *token;
-	// char *begin;
-	// int	end;
-	// int len;
-
-	// end = start;
-	// len = 0;
-	// // printf("first = %c\n", *str);
-	// while (begin)
-	// {
-	// 	if (str[start] == c)
-	// 	{
-	// 		// start++;
-	// 		end++;
-	// 		// printf("start = %c\n", str[start]);
-	// 		while(str[end++] != c)
-	// 		{
-	// 			printf("str[end] = %c\n", str[end]);
-	// 			// end++;
-	// 			len++;
-	// 		}
-	// 		// printf("end = %c\n", str[end]);
-	// 		// printf("%d\n", end);
-	// 		// printf("%d\n", start);
-	// 		info->last_position = &str[end];
-	// 		token = ft_substr(str, start, len);
-	// 		// printf("token = %s\n", token);
-	// 		return (token);
-	// 	}
-	// 	start++;
-	// 	end++;
-	// }
-	// return (NULL);
 }
 
 // char	*get_command(char *token)
@@ -284,58 +171,16 @@ char	*search_another_one(char *str, char c, t_info *info)
 void	split_token(char *token, t_info *info)
 {
 	int i;
-	// int len;
 
 	i = 0;
-	// len = 0;
 	info->last_position = token;
-	// info->token = tab_join(info->token, ft_substr(token, 0, 4));
-
-	// info->last_position = &token[4];
-	// printf("token %d = %s\n", i, info->token[i]);
-	// len +=  ft_strlen(info->token[i++]);
-	// printf("len token 1 = %i\n", len);
-	// info->token = tab_join(info->token, search_another_one(info->last_position, simple_or_double(info->last_position), info));
 
 	while (*info->last_position)
 	{
 		skip_space(info);
 		info->token = tab_join(info->token, search_another_one(info->last_position, simple_or_double(info->last_position), info));
 		skip_space(info);
-		// info->last_position++;
-		// printf("last_position = %s\n", info->last_position);
-		// print_tab(info->token);
 	}
-	// printf("token %d = %s\n", i, info->token[i]);
-
-	// len +=  ft_strlen(info->token[i++]);
-	// printf("len token 2 = %i\n", len);
-
-	
-	// info->token = tab_join(info->token, search_another_one(info->last_position, 39, info));
-	// printf("token %d = %s\n", i, info->token[i]);
-
-	// len +=  ft_strlen(info->token[i++]);
-	// printf("len token 3 = %i\n", len);
-
-
-	// print_tab(info->token);
-	// free(info->token);
-	// info->token = ft_calloc(sizeof(char **), 1);
-
-
-
-
-
-
-
-
-	// if (simple_or_double(token))
-	// 	tab = ft_split(token, simple_or_double(token));
-	// else
-	// 	tab = ft_split(token, ' ');
-
-	// return (tab);
 }
 
 void	init(t_info *info, char **envp)
--- signal.c
+++ signal.c
@@ -3,36 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   signal.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: slavoie <slavoie@student.42quebec.com>     +#+  +:+       +#+        */
+/*   By: slavoie <slavoie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 09:10:15 by cemenjiv          #+#    #+#             */
-/*   Updated: 2022/10/03 11:25:14 by slavoie          ###   ########.fr       */
+/*   Updated: 2022/10/05 15:50:26 by slavoie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 
-/*I need to free eveything that cause a segfault*/
+/*
+    I need to free eveything that cause a segfault 
+*/
 void	exit_terminal()
 { 
 	printf(" exit\n");
     exit (0);
 }
 
-/*sig_handler() function enumerated the different actions
-to be realized when SIGINT signal is sent to kernel. 
-1- Print newline on terminal
-2- rl_on_new_line tell terminal, that readline() function will be reading
-   from this new line. This function normally following a \n
-3- rl_replace_line (char *text, int clear_undo): 
-   if any text was entered prior to pressing Ctrl+C,
-   rl_replace line takes the text entered (found in buffer) 
-   and replace it by whatever is given as first argument.
-4- rl_redisplay changes what's display on screen to change for what
-   is in rl_buffer.
-*/
+/*
+    sig_handler() function enumerated the different actions
+    to be realized when SIGINT signal is sent to kernel. 
 
+    1- Print newline on terminal
+
+    2- rl_on_new_line tell terminal, that readline() function will be reading
+    from this new line. This function normally following a \n
+
+    3- rl_replace_line (char *text, int clear_undo): 
+    if any text was entered prior to pressing Ctrl+C,
+    rl_replace line takes the text entered (found in buffer) 
+    and replace it by whatever is given as first argument.
+
+    4- rl_redisplay changes what's display on screen to change for what
+    is in rl_buffer.
+*/
 void    sig_handler(int signum)
 {
 	if (signum == SIGINT)
@@ -44,12 +50,13 @@ void    sig_handler(int signum)
     }
 }
 
-// SIGINT = equivalent of Ctrl+C, SIGQUIT = equivalent of Ctrl+\
-// The signal() function defines a particular action to be realized
-// when a signal is encountered. For example, when Ctrl+C is pressed
-// The function sig_handler is immediately called whereas with the other
-// signal, it is ignored
-
+/* 
+    SIGINT = equivalent of Ctrl+C, SIGQUIT = equivalent of Ctrl+\
+    The signal() function defines a particular action to be realized
+    when a signal is encountered. For example, when Ctrl+C is pressed
+    The function sig_handler is immediately called whereas with the other
+    signal, it is ignored
+*/
 void    signal_modified()
 {
 	
@@ -57,12 +64,13 @@ void    signal_modified()
 	//signal(SIGQUIT, SIG_IGN);
 }
 
-/* This function essentially disable the ECHOCTL function
-that normally prints out ^C when Ctrl+C signal is entered on
-keyboard. We take current terminal setting with tcgetattr and 
-then modify the attributes c_lflag to disable ECHOCTL and then
-apply change with tcsetattr().*/ 
-
+/* 
+    This function essentially disable the ECHOCTL function
+    that normally prints out ^C when Ctrl+C signal is entered on
+    keyboard. We take current terminal setting with tcgetattr and 
+    then modify the attributes c_lflag to disable ECHOCTL and then
+    apply change with tcsetattr().
+*/ 
 void disable_echo()
 {
 	struct termios attributes;
