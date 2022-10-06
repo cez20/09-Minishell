ec27aa6aadf02d2346cc6fb7638185a75814a69e
Steven-emmanuel Lavoie <slavoie@c3r8p5.42quebec.com>
2022-10-06 16:21:53 -0400

    gestion des quotes pour les espaces dans echo

--- builtins.c
+++ builtins.c
@@ -3,9 +3,14 @@
 void	remove_quote(char **str)
 {
 	char	*temp;
-	char	
+	char	chr;
 
-	if (simple_or_double(*str) != 32)
+	chr = simple_or_double(*str);
+
+	// printf("%c\n", chr);
+	// printf("%c\n", str[0][ft_strlen(*str) - 2]);
+
+	if (str[0][ft_strlen(*str) - 1] == chr && chr != 32)
 	{
 		temp = *str;
 		*str = ft_substr(*str, 1, ft_strlen(*str) - 2);
@@ -23,6 +28,20 @@ void	pwd(t_info *info)
 	printf("%s\n", ++pwd);
 }
 
+/*
+		hypothèse pour gérer "" et '' coller 
+
+		flag dans la struct si oui ou non il y avait des quotes donc une struct token
+
+		flag dans la fonction
+
+		ne pas enlever les quotes, mais simplement ne pas les afficher 
+
+*/
+
+
+
+
 void	echo(t_info *info)
 {
 	int i;
@@ -34,8 +53,17 @@ void	echo(t_info *info)
 
 	while(info->token[i])
 	{
-		remove_quote(&info->token[i]);
-		printf("%s", info->token[i++]);
+		// printf("%c\n", info->token[i][0]);
+		if (info->token[i][0] == (34 | 39))
+		{
+			// remove_quote(&info->token[i]);
+			printf("%s", info->token[i++]);
+		}
+		else
+		{
+			// remove_quote(&info->token[i]);
+			printf("%s ", info->token[i++]);
+		}
 	}
 	if (ft_strncmp(info->token[1], "-n", 2))
 		printf("\n");
